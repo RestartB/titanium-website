c3a5b0d49b46181a812d32afe47c8a99a6d99986
RestartB <79792966+RestartB@users.noreply.github.com>
2026-05-29 10:27:31 +0100

    feat: add sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,11 @@
+import * as sitemap from 'super-sitemap';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+  return await sitemap.response({
+    origin: 'https://titanium.fyi',
+    excludeRoutePatterns: [
+      '.*\\/api\\/.*'
+    ]
+  });
+};
